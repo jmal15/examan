cfb68df088207a67519ee57950f26d0e31683449
minyar <jmalminyar@gmail.com>
2023-12-11 11:35:38 +0100

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
-import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import path from 'path';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { InscriptionComponent } from './inscription/inscription.component';
+
+export const routes: Routes = [
+    
+
+{path:'',component:ConnexionComponent},
+  {path:'inscription',component:InscriptionComponent},
+  { path: '', redirectTo: '/page', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+    
+}
